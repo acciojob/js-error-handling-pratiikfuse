9e5438727ae64301a8615a84ca39b1c84f9c35db
Pratik Fuse <pratikfuse20@gmail.com>
2023-12-25 11:34:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,51 @@
-//your code here
+class OutOfRangeError extends Error{
+	constructor(message){
+		super(message);
+		this.message = message;
+		this.name  = "OutOfRangeError"
+	}
+}
+
+class InvalidExprError extends Error{
+	constructor(message){
+		super(message);
+		this.message = message;
+		this.name  = "InvalidExprError";
+	}
+}
+
+function isOperator(op){
+  if(op == '-' || op == '/' ||op == '*' || op == '+'){
+    return true;
+  }
+  return false;
+}
+
+function evalString(str){
+	try{
+	  for(let i = 0;i<str.length;i++){
+	    if((str[i]>=0 && str[i]<=9) || str[i] === ' '  || isOperator(str[i])){
+	      if(isOperator(str[i])){
+	        if(i == 0){
+	          throw new SyntaxError("Expression should not start with invalid operator");
+	        }
+	        if(i == str.length-1){
+	          throw new SyntaxError("Expression should not end with invalid operator");
+	        }
+	      }
+	       if(i != str.length-1){
+	         if(isOperator(str[i]) && isOperator(str[i+1])){
+	           throw new InvalidExprError("Expression should not have an invalid combination of expression");
+	         }
+	       }
+	    }
+	    else{
+	      throw new OutOfRangeError("Expression should only consist of integers and +-/* characters and not "+str[i]);
+	    }
+	  
+	}
+	}
+	catch(e){
+	  console.log(e.message);
+	}
+}
